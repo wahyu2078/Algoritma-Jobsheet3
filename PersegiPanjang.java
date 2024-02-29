56eeaf541fb338c6dd3a79a353f2762eea112cd6
wahyu2078 <wahyutptp20@gmail.com>
2024-02-29 21:27:56 +0700

    Jobsheet3

--- PersegiPanjang.java
+++ PersegiPanjang.java
@@ -1,10 +1,21 @@
 public class PersegiPanjang {
-        int panjang;
-        int lebar;
+    int panjang;
+    int lebar;
 
-        public PersegiPanjang(int p, int l) {
-                panjang = p;
-                lebar = l;
-        }
+    public PersegiPanjang(int p, int l) {
+        panjang = p;
+        lebar = l;
+    }
 
+    public void cetakInfo() {
+        System.out.println("Panjang: " + panjang + ", lebar: " + lebar);
+    }
+
+    public int hitungLuas() {
+        return panjang * lebar;
+    }
+
+    public int hitungKeliling() {
+        return 2 * (panjang + lebar);
+    }
 }
